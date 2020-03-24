a040e7a930b8eb8f83f992da4053bf0b6cc7de31
AndreaBegni <braintechno1@gmail.com>
2020-03-24 14:04:01 +0100

    add routes and template pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Signin from "./Signin";
+import Signup from "./Signup";
+import Dashboard from "./Dashboard";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route path="/signup" component={Signup} />
+      <Route path="/signin" component={Signin} />
+      <Route path="/dashboard" component={Dashboard} />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
